958b67962aab077da491def9b6126da113993a14
ivbeck <ivbeck@mail.uni-mannheim.de>
2023-04-18 20:35:44 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,22 @@
 import { useState } from 'react';
 import classNames from 'classnames';
-import { ReactComponent as ReactLogo } from './assets/react.svg';
-import { ReactComponent as ViteLogo } from './assets/vite.svg';
-import { ReactComponent as TypescriptLogo } from './assets/typescript.svg';
-import { ReactComponent as ScssLogo } from './assets/scss.svg';
+import { ReactComponent as GithubLogo } from './assets/github.svg';
+import { ReactComponent as LinkedInLogo } from './assets/linkedin.svg';
 import styles from './App.module.scss';
 
 function App() {
-    const [count, setCount] = useState(0);
-
     return (
         <div className={styles.App}>
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <ViteLogo
-                        height="6em"
-                        width="6em"
-                        className={classNames(styles.logo)}
-                        title="Vite logo"
-                    />
+            <div className={styles.title}>Iven Beck</div>
+            <div className={styles.welcome}>Welcome to my site.</div>
+            <div className={styles.logos}>
+                <a href="https://github.com/ibveecnk" className={styles.logo}>
+                    <GithubLogo />
                 </a>
-                <a href="https://reactjs.org" target="_blank">
-                    <ReactLogo
-                        height="6em"
-                        width="6em"
-                        className={classNames(styles.logo, styles.react)}
-                        title="React logo"
-                    />
-                </a>
-                <a href="https://www.typescriptlang.org/" target="_blank">
-                    <TypescriptLogo
-                        height="6em"
-                        width="6em"
-                        className={classNames(styles.logo, styles.ts)}
-                        title="Typescript logo"
-                    />
-                </a>
-                <a href="https://sass-lang.com/" target="_blank">
-                    <ScssLogo
-                        height="6em"
-                        width="6em"
-                        className={classNames(styles.logo, styles.scss)}
-                        title="SCSS logo"
-                    />
+                <a href="https://linkedin.com/in/ipbeck" className={styles.logo}>
+                    <LinkedInLogo />
                 </a>
             </div>
-            <div className={styles.card}>
-                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className={styles['read-the-docs']}>
-                Click on the Vite and React logos to learn more
-            </p>
         </div>
     );
 }
